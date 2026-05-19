d11a8e99d84f2a2e1cb8e1b3113066c07f668b27
Fagghino <fgiordano839@gmail.com>
2026-05-19 14:14:19 +0200

    feat(registration): register Advanced Empowerer block entity

---
+++ src/main/java/com/actuallyadditions/advancedmachinery/registration/ModBlockEntities.java
@@ -0,0 +1,15 @@
+package com.actuallyadditions.advancedmachinery.registration;
+
+import com.actuallyadditions.advancedmachinery.AdvancedMachinery;
+import com.actuallyadditions.advancedmachinery.blockentity.AdvancedEmpowererBlockEntity;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModBlockEntities {
+    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, AdvancedMachinery.MODID);
+
+    public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<AdvancedEmpowererBlockEntity>> ADVANCED_EMPOWERER =
+            BLOCK_ENTITIES.register("advanced_empowerer", () -> BlockEntityType.Builder.of(AdvancedEmpowererBlockEntity::new, ModBlocks.ADVANCED_EMPOWERER.get()).build(null));
+}
