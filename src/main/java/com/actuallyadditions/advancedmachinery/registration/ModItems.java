6a934b5dd89e58ec13aa736a5b466d8573b29af3
Fagghino <fgiordano839@gmail.com>
2026-05-19 14:14:21 +0200

    feat(registration): register speed and efficiency upgrades and block items

---
+++ src/main/java/com/actuallyadditions/advancedmachinery/registration/ModItems.java
@@ -0,0 +1,16 @@
+package com.actuallyadditions.advancedmachinery.registration;
+
+import com.actuallyadditions.advancedmachinery.AdvancedMachinery;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(AdvancedMachinery.MODID);
+
+    public static final DeferredItem<BlockItem> ADVANCED_EMPOWERER = ITEMS.registerSimpleBlockItem(ModBlocks.ADVANCED_EMPOWERER);
+
+    public static final DeferredItem<Item> SPEED_UPGRADE = ITEMS.registerSimpleItem("speed_upgrade", new Item.Properties());
+    public static final DeferredItem<Item> EFFICIENCY_UPGRADE = ITEMS.registerSimpleItem("efficiency_upgrade", new Item.Properties());
+}
